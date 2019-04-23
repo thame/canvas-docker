40a53ecbb6a65a94005379b5109b6c4ce6a1bcbe
Tom Fadial <tomfadial@gmail.com>
2019-04-22 20:08:02 -0500

    Rails CORS

--- assets/development-local.rb
+++ assets/development-local.rb
@@ -1,3 +1,7 @@
 config.cache_classes = true
 config.action_controller.perform_caching = true
 config.action_view.cache_template_loading = true
+config.action_dispatch.default_headers = {
+  'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+  'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+}
